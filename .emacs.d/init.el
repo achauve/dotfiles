95ccc49a461aa6d078350b21e7c05ad61030bbd7
Adrien Chauve <adrien.chauve@gmail.com>
2011-05-20 16:50:33 +0200

    Add org-mode config to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -46,6 +46,13 @@
 ;;;;;;;;;;;;;;;;;;;;;;;End grep
 
 
+;;;;;;;;;;;;;; org-mode
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
 
 
 ;;;;;;;;;;;;;;;;;
